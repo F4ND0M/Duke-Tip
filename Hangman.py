0e38445e3c2d932b82de42a1b03be78c363dff14
joy <thebirdfliesaway@gmail.com>
2017-07-14 11:35:10 -0400

    add Hangman

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,6 @@
 import numpy
 
-words = ["dog", "cat", "chicken", "snake", "sheep"]
+words = ["dog", "cat", "rooster", "snake", "sheep", "monkey", "pig", "horse", "dragon", "rabbit", "cow", 'mouse', 'tiger']
 correctWord = numpy.random.choice(words)
 
 space = '_' * len(correctWord)
@@ -21,17 +21,19 @@ for turn in range(1, 30):
         print space
     letter = raw_input('Guess a letter:')
     for wrong in range(1, 6):
-        wrong = letter not in correctWord
-    #if letter is not str :
+        wrong = letter not in correctWord #number of times letter not in correctWord
+    #if letter is not str : ADJUST AND ADD ONE FOR WORDS WITH DOUBLE LETTERS, ALREADY CHOSEN LETTERS, SPACING, WHEN WORD IS COMPLETED, MORE THAN ONE LETTER TYPED
      #   print "This is not a letter."
         #print "You've already choose this letter"
     if letter in correctWord:
         print "Correct!"
-        if space <= len(correctWord):
-            space = space[:-1]
+        if space <= len(correctWord): #ADJUST
+            space = space[:-2]
         space = bytearray(space)
         value = correctWord.index(letter)
         space[value] = letter
+        #if len(correctWord) =
+        #if letter in correctWord
         print space
         print hanger()
     elif wrong == 1:
@@ -44,6 +46,7 @@ for turn in range(1, 30):
         print'    |'
         print'    |'
         print'---------'
+        print space
     elif wrong == 2:
         print "Incorrect."
         print'    |-----'
