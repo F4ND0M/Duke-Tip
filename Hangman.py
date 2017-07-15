8874519eb6a7aeadab33b8423187660681520f6d
F4ND0M <thebirdfliesaway@gmail.com>
2017-07-15 06:52:09 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -110,3 +110,77 @@ def man(wrong):
         print'---------'
         print 'You lost.'
         print 'The word was ' + correctWord + '.'
+        
+        
+        
+        import numpy
+
+words = ["dog", "cat"] #"rooster", "snake", "sheep", "monkey", "pig", "horse", "dragon", "rabbit", "cow", 'mouse', 'tiger']
+correctWord = numpy.random.choice(words)
+guesses = []
+# if letter is not str : ADJUST AND ADD ONE FOR WORDS WITH DOUBLE LETTERS, SPACING, WHEN WORD IS COMPLETED, MORE THAN ONE LETTER TYPED
+
+space = '_ ' * len(correctWord)
+
+def hanger():
+    print'    |-----'
+    print'    |    |'
+    print'    |'
+    print'    |'
+    print'    |'
+    print'    |'
+    print'    |'
+    print'---------'
+wrongGuess = 0
+
+def hangman(wrong):
+    if wrong >= 1:
+        print "Incorrect."
+        print'    |-----'
+        print'    |    |'
+        print'    |    O'
+    else:
+        print'    |-----'
+        print'    |    |'
+        print'    |'
+    if wrong == 2:
+        print'    |    |'
+    else:
+        print'    |'
+    if wrong == 3:
+        print'    |   /|'
+    if wrong >= 4:
+        print'    |   /|\\'
+    else:
+        print'    |'
+    if wrong == 5:
+        print'    |   /'
+    elif wrong >= 6:
+        print'    |   / \\'
+    else:
+        print'    |'
+    print'    |'
+    print'    |'
+    print'---------'
+
+
+
+for turn in range(1, 7):
+    if turn == 1:
+    letter = raw_input('Guess a letter:')
+    if letter in guesses:
+        print "You've already choose this letter."
+    guesses.append(letter)
+    if letter in correctWord:
+        print "Correct!"
+        space = bytearray(space)
+        if correctWord.index(letter) == 1:
+            value = correctWord.index(letter)
+        else:
+            value = correctWord.index(letter) + 2
+        space[value] = letter
+        print space
+        print hanger()
+    if letter not in correctWord:
+        wrongGuess += 1
+hangman(wrongGuess)
